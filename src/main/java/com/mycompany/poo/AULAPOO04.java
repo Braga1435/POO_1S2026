0d45530a26dbd542bf2735283c54ac4c7e56f184
WALISSON ALVES BRAGA <walisson.braga@a.ucb.br>
2026-03-08 13:47:35 -0300

    Adicionando AULAPOO04

---
+++ src/main/java/com/mycompany/poo/AULAPOO04.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo;
+
+/**
+ *
+ * @author walis
+ */
+public class AULAPOO04 {
+    public static void main(String[] args) {
+        System.out.println("Aqui vamos trabalhar os conteudos da aula de terca dia 10 de marco de 2026");
+    }
+}
