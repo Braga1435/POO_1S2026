ee215ca396b109d410fb1b1bb55abda16d03a660
WALISSON ALVES BRAGA <walisson.braga@a.ucb.br>
2026-03-08 14:45:23 -0300

    Testando o sincronismo com o git

---
+++ src/main/java/com/mycompany/poo/TESTANDOTERMINAL02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo;
+
+/**
+ *
+ * @author walis
+ */
+public class TESTANDOTERMINAL02 {
+    public static void main(String[] args) {
+        System.out.println("Testando o terminal uma ultima vez");
+    }
+    
+}
