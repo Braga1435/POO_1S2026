7ce911672befa32a63ba6490bbc08b57dfae6a54
WALISSON ALVES BRAGA <walisson.braga@a.ucb.br>
2026-03-08 14:13:42 -0300

    Adicionando AULAPOO05

---
+++ src/main/java/com/mycompany/poo/AULAPOO05.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo;
+
+/**
+ *
+ * @author walis
+ */
+public class AULAPOO05 {
+    public static void main(String[] args) {
+         System.out.println("Bem vindo a nossa quinta aula de POO");
+    }
+    
+}
